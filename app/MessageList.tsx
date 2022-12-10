abfb9fda1c5081e5a6066caeebd503302d91d585
oklar <email>
2022-12-10 20:31:47 +0100

    added initialmessages to messagelist; added nextjs loading

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -7,7 +7,11 @@ import { Message } from "../typings";
 import fetcher from "../utils/fetchMessages";
 import MessageComponent from "./MessageComponent";
 
-function MessageList() {
+type Props = {
+  initialMessages: Message[];
+};
+
+function MessageList({ initialMessages }: Props) {
   const {
     data: messages,
     error,
@@ -38,7 +42,7 @@ function MessageList() {
 
   return (
     <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
-      {messages?.map((message) => (
+      {(messages || initialMessages)?.map((message) => (
         <div key={message.id}>
           <MessageComponent key={message.id} message={message} />
         </div>
