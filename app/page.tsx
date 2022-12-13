d8646e176a1d339a2676c2c4f47a92e54886d873
oklar <email>
2022-12-13 19:59:36 +0100

    fix redirect

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,19 @@ import { Message } from "../typings";
 import ChatInput from "./ChatInput";
 import MessageList from "./MessageList";
 import { Providers } from "./providers";
+import { redirect } from "next/navigation";
 
 async function HomePage() {
+  const session = await unstable_getServerSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   const data = await fetch(
     `${process.env.VERCEL_URL || "http://localhost:3000"}/api/getMessages`
   ).then((res) => res.json());
 
-  const session = await unstable_getServerSession();
   const messages: Message[] = data.messages;
 
   return (
