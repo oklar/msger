365ad7d46b165a5dc4e39bc3f8bb028b4ad7584d
oklar <email>
2022-12-14 01:39:57 +0100

    https

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,9 @@ async function HomePage() {
   }
 
   const data = await fetch(
-    `${process.env.VERCEL_URL || "http://localhost:3000"}/api/getMessages`
+    `https://${
+      process.env.VERCEL_URL || "http://localhost:3000"
+    }/api/getMessages`
   ).then((res) => res.json());
 
   const messages: Message[] = data.messages;
